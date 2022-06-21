7fe0cafd5d5c6a31cea1bbfa26c27804e06e6fa0
gyan <1700010674@pku.edu.cn>
2022-06-21 14:16:08 -0700

    Add experiments 1 & 2

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import time
 from tqdm import tqdm 
-from utils import * 
+import utils 
 from conformal import ConformalModel
 import torch.backends.cudnn as cudnn
 import random
@@ -20,6 +20,9 @@ parser.add_argument('--num_workers', metavar='NW', help='number of workers', def
 parser.add_argument('--num_calib', metavar='NCALIB', type=int, help='number of calibration points', default=10000)
 parser.add_argument('--seed', metavar='SEED', help='random seed', default=0)
 parser.add_argument('--eps', type=float, default=2/255)
+parser.add_argument('--exp1', action='store_true', default=False)
+parser.add_argument('--exp2', action='store_true', default=False)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -46,22 +49,18 @@ if __name__ == "__main__":
     val_loader = torch.utils.data.DataLoader(val_data_subset, batch_size=args.val_batch_size, shuffle=True, pin_memory=True)
 
     cudnn.benchmark = True
+    # PGD attack images
+    def get_adv_loaders(model):
+        def adv_collate(model, batch):
+            img, target = zip(*batch)
+            img = torch.stack(img)
+            target = torch.Tensor(target).cuda().type(torch.long)
+            adv_img = utils.pgd_attack(model, img, target, torch.device('cuda'), eps=args.eps)
+            return adv_img, target
 
-    # Get the model 
-    model = get_model(args.arch)
-    model = torch.nn.DataParallel(model) 
-    model.eval()
-
-    def adv_collate(batch):
-        img, target = zip(*batch)
-        img = torch.stack(img)
-        target = torch.Tensor(target).cuda().type(torch.long)
-        adv_img = pgd_attack(model, img, target, torch.device('cuda'), eps=args.eps)
-        return adv_img, target
-
-    adv_loader = torch.utils.data.DataLoader(val_data_subset, batch_size=args.val_batch_size, shuffle=True, collate_fn=adv_collate)
-    adv_calib_loader = torch.utils.data.DataLoader(imagenet_calib_data, batch_size=args.val_batch_size, shuffle=True, collate_fn=adv_collate)
-
+        adv_loader = torch.utils.data.DataLoader(val_data_subset, batch_size=args.val_batch_size, shuffle=True, collate_fn=lambda x: adv_collate(model, x))
+        adv_calib_loader = torch.utils.data.DataLoader(imagenet_calib_data, batch_size=args.val_batch_size, shuffle=True, collate_fn=lambda x: adv_collate(model, x))
+        return adv_loader, adv_calib_loader
     # Sanity check
     def evaluate(model, dataloader):
         num_imgs, correct = 0, 0
@@ -72,8 +71,15 @@ if __name__ == "__main__":
             correct += torch.sum(prediction == label)
             num_imgs += label.shape[0]
         print("Avg acc: {:.3f}".format(correct/num_imgs))
-
-
+    # Validation function
+    def validate(val_loader, adv_loader, cmodel):
+        print("Clean data validation.")
+        print("Model calibrated and conformalized! Now evaluate over remaining data.")
+        print("Normal Test.")
+        print("Result:{} {} {} {}.".format(*utils.validate(val_loader, cmodel, print_bool=True)))
+        print("ADV Test.")
+        print("Result:{} {} {} {}.".format(*utils.validate(adv_loader, cmodel, print_bool=True)))
+        print("Complete!")
     # optimize for 'size' or 'adaptiveness'
     lamda_criterion = 'size'
     # allow sets of size zero
@@ -82,8 +88,26 @@ if __name__ == "__main__":
     randomized = True 
 
     # Conformalize model
-    cmodel = ConformalModel(model, adv_calib_loader, alpha=0.1, lamda_criterion='size')
-    print("Model calibrated and conformalized! Now evaluate over remaining data.")
-    # validate(val_loader, cmodel, print_bool=True)
-    validate(adv_loader, cmodel, print_bool=True)
-    print("Complete!")
+    if args.exp1:
+        model = utils.get_model(args.arch)
+        model.eval()
+        adv_loader, adv_calib_loader = get_adv_loaders(model)
+        cmodel_clean = ConformalModel(model, calib_loader, alpha=0.1, lamda_criterion='size')
+        print("Validate clean data model.")
+        validate(val_loader, adv_loader, cmodel_clean)
+        cmodel_adv = ConformalModel(model, adv_calib_loader, alpha=0.1, lamda_criterion='size')
+        print("Validate adv data model.")
+        validate(val_loader, adv_loader, cmodel_adv)
+    if args.exp2:
+        # Could adversarial trained model be robust?
+        adv_model = utils.get_model("ResNet50")
+        adv_model.load_state_dict(torch.load("ckpt/imagenet_model_weights_2px.pth.tar")['state_dict'])
+        adv_model.eval()
+        adv_model = torch.nn.DataParallel(adv_model).cuda()
+        adv_loader, adv_calib_loader = get_adv_loaders(adv_model)
+        cmodel_clean = ConformalModel(adv_model, calib_loader, alpha=0.1, lamda_criterion='size')
+        print("Validate clean data model.")
+        validate(val_loader, adv_loader, cmodel_clean)
+        cmodel_adv = ConformalModel(adv_model, adv_calib_loader, alpha=0.1, lamda_criterion='size')
+        print("Validate adv data model.")
+        validate(val_loader, adv_loader, cmodel_adv)
